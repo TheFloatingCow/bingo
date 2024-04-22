8327a6696e19713a1e2410181e93f1e42535bb65
Kenji MacMartin <kenji_macmartin@sfu.ca>
2024-04-21 23:01:21 -0700

    Edit button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 import classes from './Button.module.css';
 
-const button = (props) => (
-    <button
-        disabled={props.disabled}
-        type="button"
-        className={[classes.Button, classes[props.btnType]].join(' ')}
-        onClick={props.clicked}>{props.children}</button>
-);
+const button = (props) => {
+    
+    const {btnType} = props;
+
+    return (
+        <button
+            disabled={props.disabled}
+            type="button"
+            className={`${classes.Button} ${btnType}`}
+            onClick={props.clicked}>{props.children}</button>
+)
+    
+};
 
 export default button;
