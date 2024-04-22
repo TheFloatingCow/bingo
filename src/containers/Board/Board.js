b73f863d03f1a8fba924926d42a96d32adcc8c0d
Kenji MacMartin <kenji_macmartin@sfu.ca>
2024-04-21 23:50:39 -0700

    Change Board.js to functional component

--- src/containers/Board/Board.js
+++ src/containers/Board/Board.js
@@ -1,82 +1,81 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import classes from './Board.module.css'; //import classes
 import Aux from '../../hoc/Auxiliary/Auxiliary';
 import Box from '../../components/UI/Box/Box';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid
 
-class Board extends Component {
+const Board = () => {
 
     /*
     Data types:
     Monthly, Yearly
     General, Academic, Driving, Sports,
     */
-    state = {
+    const [data, setData] = useState({
         data: {
             monthly: {
                 general: [
-                    "Challenge 1",
-                    "Challenge 2"
+                    "general 1",
+                    "general 2"
                 ],
                 academic: [
-                    "Challenge 1",
-                    "Challenge 2"
+                    "academic 1",
+                    "academic 2"
                 ],
                 driving: [
-                    "Challenge 1",
-                    "Challenge 2"
+                    "driving 1",
+                    "driving 2"
                 ]
             },
             yearly: {
                 general: [
-                    "Challenge 1",
-                    "Challenge 2"
+                    "general 1",
+                    "general 2"
                 ],
                 academic: [
-                    "Challenge 1",
-                    "Challenge 2"
+                    "academic 1",
+                    "academic 2"
                 ],
                 driving: [
-                    "Challenge 1",
-                    "Challenge 2"
-                ] 
+                    "driving 1",
+                    "driving 2"
+                ]
             }
+        }
+    })
+
+
+
+    let boardList = [];
+    let key = 0;
+
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            key++;
+            boardList.push(
+                <Box
+                    key={key}
+                    xs={2.4}>
+                    {key === 13 ? "FREEEEE\n" : "content\n"}
+                    {i + 1} {j + 1}</Box>
+            )
         }
     }
 
-    render () {
-        
-        let boardList = [];
-        let key = 0;
+    let board = (
+        <Aux>
+            <Grid className={classes.Container} container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 1 }}>
+                {boardList}
+            </Grid>
+        </Aux>
+    )
 
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 5; j++) {
-                key++;
-                boardList.push(
-                    <Box
-                        key={key}
-                        xs={2.4}>
-                            {key === 13 ? "FREEEEE\n" : "content\n"}
-                            {i+1} {j+1}</Box>
-                )
-            }
-        }
-
-        let board = (
-            <Aux>
-                <Grid className={classes.Container} container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 1 }}>
-                    {boardList}
-                </Grid>
-            </Aux>
-        )
-
-        return (
-            <Aux>
-                {board}
-            </Aux>
-        )
-    }
+    return (
+        <Aux>
+            {board}
+        </Aux>
+    )
 }
 
 export default Board;
