4d15f400eda1966583a955f7966123b37a357f50
Kenji MacMartin <kenji_macmartin@sfu.ca>
2024-04-24 23:31:42 -0700

    Move CheckboxMenu to new component

---
+++ src/components/UI/CheckboxMenu/CheckboxMenu.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+import classes from './CheckboxMenu.module.css';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Box from '@mui/material/Box';
+
+const CheckboxMenu = (props) => {
+
+    const { types } = props;
+
+    let keys = Object.keys(types)
+
+    const [checked, setChecked] = useState(Array(keys.length - 1).fill(true));
+
+    // Handle the Select all checkbox
+    const handleParentChange = (event) => {
+        setChecked(Array.from({ length: checked.length }, () => event.target.checked));
+    };
+
+    // Handle individual checkboxes
+    const handleChange = (event, key) => {
+        setChecked(checked => {
+            const newChecked = [...checked];
+            newChecked[key] = event.target.checked;
+            return newChecked;
+        });
+    };
+    
+    // Capitalize the first letter of word
+    const capitalize = (word) => {
+        return word[0].toUpperCase() + word.slice(1);
+    }
+    
+    // Create checklist
+    let checklist = [];
+
+    for (let i = 1; i < keys.length; i++) {
+        checklist.push(
+            <FormControlLabel
+                key={i - 1}
+                label={capitalize(keys[i])}
+                control={<Checkbox
+                    key={i - 1}
+                    checked={checked[i - 1]}
+                    onChange={(event) => handleChange(event, i - 1)}
+                    size="large" />} />
+        )
+    }
+
+    const children = (
+        <Box sx={{ display: 'flex', flexDirection: 'column', ml: 3 }}>
+            {checklist}
+        </Box>
+    );
+
+    return (
+        <Aux className={classes.CheckboxMenu}>
+            <FormGroup>
+                <FormControlLabel label="Select all" control={
+                    <Checkbox
+                        checked={checked.every(element => element === true)}
+                        indeterminate={!(checked.every(element => element === true)) && !checked.every(element => element === false)}
+                        onChange={handleParentChange}
+                        size="large" />
+                } />
+            </FormGroup>
+            {children}
+        </Aux>
+    )
+}
+
+export default CheckboxMenu;
--- src/containers/EditModal/EditModal.js
+++ src/containers/EditModal/EditModal.js
@@ -2,76 +2,34 @@ import React, { useState } from 'react';
 
 import classes from './EditModal.module.css';
 import Aux from '../../hoc/Auxiliary/Auxiliary';
-import Checkbox from '@mui/material/Checkbox';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Box from '@mui/material/Box';
-import ToggleButton from "../../components/UI/ToggleButton/ToggleButton";
+import CheckboxMenu from "../../components/UI/CheckboxMenu/CheckboxMenu";
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+//import ToggleButton from "../../components/UI/ToggleButton/ToggleButton";
 
 const EditModal = (props) => {
 
     const { types } = props;
 
-    let keys = Object.keys(types)
+    const [alignment, setAlignment] = useState('web');
 
-    const [checked, setChecked] = useState(Array(keys.length - 1).fill(true));
-
-    // Handle the Select all checkbox
-    const handleParentChange = (event) => {
-        setChecked(Array.from({ length: checked.length }, () => event.target.checked));
+    const handleToggle = (event, newAlignment) => {
+        setAlignment(newAlignment);
     };
-
-    // Handle individual checkboxes
-    const handleChange = (event, key) => {
-        setChecked(checked => {
-            const newChecked = [...checked];
-            newChecked[key] = event.target.checked;
-            return newChecked;
-        });
-    };
-
-
-    // Capitalize the first letter of word
-    const capitalize = (word) => {
-        return word[0].toUpperCase() + word.slice(1);
-    }
-
-    // Create checklist
-    let checklist = [];
-
-    for (let i = 1; i < keys.length; i++) {
-        checklist.push(
-            <FormControlLabel
-                key={i - 1}
-                label={capitalize(keys[i])}
-                control={<Checkbox
-                    key={i - 1}
-                    checked={checked[i - 1]}
-                    onChange={(event) => handleChange(event, i - 1)}
-                    size="large" />} />
-        )
-    }
-
-    const children = (
-        <Box sx={{ display: 'flex', flexDirection: 'column', ml: 3 }}>
-            {checklist}
-        </Box>
-    );
 
     return (
         <Aux className={classes.EditModal}>
             EditModal contents
-            <ToggleButton />
-            <FormGroup>
-                <FormControlLabel label="Select all" control={
-                    <Checkbox
-                        checked={checked.every(element => element === true)}
-                        indeterminate={!(checked.every(element => element === true)) && !checked.every(element => element === false)}
-                        onChange={handleParentChange}
-                        size="large" />
-                } />
-            </FormGroup>
-            {children}
+            <ToggleButtonGroup
+                color="primary"
+                value={alignment}
+                exclusive
+                onChange={handleToggle}
+                aria-label="Platform" >
+                <ToggleButton value="web">Monthly</ToggleButton>
+                <ToggleButton value="android">Yearly</ToggleButton>
+            </ToggleButtonGroup>
+            <CheckboxMenu types={types} />
         </Aux>
     )
 }
