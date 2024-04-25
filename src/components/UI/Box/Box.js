4769e2806e127242601b5d3e2d3a54b894356c8e
Kenji MacMartin <kenji_macmartin@sfu.ca>
2024-04-24 20:45:25 -0700

    Add return statement to Box

--- src/components/UI/Box/Box.js
+++ src/components/UI/Box/Box.js
@@ -4,13 +4,16 @@ import classes from './Box.module.css';
 
 import Grid from '@mui/material/Unstable_Grid2'; // Grid
 
-const box = (props) => (
-    <Grid
-        index={props.index}
-        className={classes.Box}
-        xs={props.xs}
-        minHeight={props.height}
-        onClick={props.clicked}>{props.children}</Grid>
-);
+const box = (props) => {
+
+    return (
+        <Grid
+            index={props.index}
+            className={classes.Box}
+            xs={props.xs}
+            minHeight={props.height}
+            onClick={props.clicked}>{props.children}</Grid>
+    )
+}
 
 export default box;
