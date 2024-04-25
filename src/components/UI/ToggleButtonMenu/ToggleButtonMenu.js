febee0de564df94b60c7cedb5d0f5ec379de796d
Kenji MacMartin <kenji_macmartin@sfu.ca>
2024-04-25 00:01:43 -0700

    Complete moving ToggleButtonMenu

--- src/components/UI/ToggleButtonMenu/ToggleButtonMenu.js
+++ src/components/UI/ToggleButtonMenu/ToggleButtonMenu.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './ToggleButtonMenu.module.css';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 const ToggleButtonMenu = (props) => {
 
+    const [alignment, setAlignment] = useState('web');
+
+    const handleChange = (event, newAlignment) => {
+        setAlignment(newAlignment);
+    };
+
     return (
-        <Aux>
+        <Aux className={classes.ToggleButtonMenu}>
             <ToggleButtonGroup
                 color="primary"
                 value={alignment}
                 exclusive
-                onChange={handleToggle}
+                onChange={handleChange}
                 aria-label="Platform" >
                 <ToggleButton value="web">Monthly</ToggleButton>
                 <ToggleButton value="android">Yearly</ToggleButton>
--- src/containers/EditModal/EditModal.js
+++ src/containers/EditModal/EditModal.js
@@ -1,34 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import classes from './EditModal.module.css';
 import Aux from '../../hoc/Auxiliary/Auxiliary';
 import CheckboxMenu from "../../components/UI/CheckboxMenu/CheckboxMenu";
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-//import ToggleButton from "../../components/UI/ToggleButton/ToggleButton";
+import ToggleButtonMenu from "../../components/UI/ToggleButtonMenu/ToggleButtonMenu";
 
 const EditModal = (props) => {
 
     const { types } = props;
 
-    const [alignment, setAlignment] = useState('web');
-
-    const handleToggle = (event, newAlignment) => {
-        setAlignment(newAlignment);
-    };
-
     return (
         <Aux className={classes.EditModal}>
             EditModal contents
-            <ToggleButtonGroup
-                color="primary"
-                value={alignment}
-                exclusive
-                onChange={handleToggle}
-                aria-label="Platform" >
-                <ToggleButton value="web">Monthly</ToggleButton>
-                <ToggleButton value="android">Yearly</ToggleButton>
-            </ToggleButtonGroup>
+            <ToggleButtonMenu />
             <CheckboxMenu types={types} />
         </Aux>
     )
