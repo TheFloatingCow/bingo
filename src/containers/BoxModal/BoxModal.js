e66687ebe91fe9ae0c0a39d2cf20879eb1b5b0e1
Kenji MacMartin <kenji_macmartin@sfu.ca>
2024-04-24 20:45:13 -0700

    Create BoxModal

---
+++ src/containers/BoxModal/BoxModal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './BoxModal.module.css';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
+
+const BoxModal = () => {
+
+    return (
+        <Aux className={classes.BoxModal}>
+            BoxModal contents
+        </Aux>
+    )
+}
+
+export default BoxModal;
